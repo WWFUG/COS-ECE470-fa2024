e4025531fe224d80c5aea21a94cc9b75b8a613a0
Yu-Wei Fan <yf9172@compton.princeton.edu>
2024-09-13 14:32:28 -0400

    Yu-Wei: transaction.rs finished

--- src/types/transaction.rs
+++ src/types/transaction.rs
@@ -1,28 +1,50 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature};
+use ring::signature::{Ed25519KeyPair, Signature, UnparsedPublicKey, ED25519};
 use rand::Rng;
+use super::address::Address;
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Transaction {
+    sender: Address,
+    receiver: Address,
+    value: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct SignedTransaction {
+    transaction: Transaction,
+    signature: Vec<u8>,
+    public_key: Vec<u8>,
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let encoded_trans = bincode::serialize(&t).unwrap();
+    key.sign(&encoded_trans[..])
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &[u8], signature: &[u8]) -> bool {
-    unimplemented!()
+    let encoded_trans = bincode::serialize(&t).unwrap();
+    let decoded_pub_key = UnparsedPublicKey::new(&ED25519, public_key);
+    decoded_pub_key.verify(&encoded_trans, signature).is_ok()
 }
 
 #[cfg(any(test, test_utilities))]
 pub fn generate_random_transaction() -> Transaction {
-    unimplemented!()
+    let mut rng = rand::thread_rng();
+    let mut sender = [0u8; 20];
+    let mut receiver = [0u8; 20];
+    let value : u32 = rng.gen::<u32>();
+    rng.fill(&mut sender);
+    rng.fill(&mut receiver);
+
+    Transaction{
+        sender: Address::from(sender),
+        receiver: Address::from(receiver),
+        value: value, // Random value between 1 and 1000
+    }
+    
 }
 
 // DO NOT CHANGE THIS COMMENT, IT IS FOR AUTOGRADER. BEFORE TEST
