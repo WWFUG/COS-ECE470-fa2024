84c6bdb54710c08471e0b188445deeb882c87d6d
Chenyungchin <b08901061@ntu.edu.tw>
2024-10-06 02:45:43 -0400

    Jim: finish part4

--- src/miner/mod.rs
+++ src/miner/mod.rs
@@ -7,7 +7,11 @@ use std::time;
 
 use std::thread;
 
-use crate::types::block::Block;
+use crate::types::block::{Header, Block, Content};
+use std::sync::{Arc, Mutex};
+use crate::blockchain::Blockchain;
+use crate::types::hash::{H256, Hashable};
+use crate::types::merkle::MerkleTree;
 
 enum ControlSignal {
     Start(u64), // the number controls the lambda of interval between block generation
@@ -26,6 +30,7 @@ pub struct Context {
     control_chan: Receiver<ControlSignal>,
     operating_state: OperatingState,
     finished_block_chan: Sender<Block>,
+    blockchain: Arc<Mutex<Blockchain>>,
 }
 
 #[derive(Clone)]
@@ -34,14 +39,16 @@ pub struct Handle {
     control_chan: Sender<ControlSignal>,
 }
 
-pub fn new() -> (Context, Handle, Receiver<Block>) {
+pub fn new(blockchain: &Arc<Mutex<Blockchain>>) -> (Context, Handle, Receiver<Block>) {
     let (signal_chan_sender, signal_chan_receiver) = unbounded();
     let (finished_block_sender, finished_block_receiver) = unbounded();
+    let blockchain_cloned = Arc::clone(blockchain);
 
     let ctx = Context {
         control_chan: signal_chan_receiver,
         operating_state: OperatingState::Paused,
         finished_block_chan: finished_block_sender,
+        blockchain: blockchain_cloned,
     };
 
     let handle = Handle {
@@ -53,7 +60,9 @@ pub fn new() -> (Context, Handle, Receiver<Block>) {
 
 #[cfg(any(test,test_utilities))]
 fn test_new() -> (Context, Handle, Receiver<Block>) {
-    new()
+    let blockchain = Blockchain::new();
+    let blockchain = Arc::new(Mutex::new(blockchain));
+    new(&blockchain)
 }
 
 impl Handle {
@@ -84,6 +93,17 @@ impl Context {
     }
 
     fn miner_loop(&mut self) {
+        let mut parent = H256::default();
+        match self.blockchain.lock() {
+            Ok(blockchain_guard) => {
+                // println!("Obtained!");
+                parent = blockchain_guard.tip();
+            }
+            Err(poisoned) => {
+                println!("The mutex is poisoned! Can't safely access the blockchain.");
+            }
+        }
+
         // main mining loop
         loop {
             // check and react to control signals
@@ -133,7 +153,36 @@ impl Context {
             }
 
             // TODO for student: actual mining, create a block
+            let difficulty = [255u8; 32].into();
+            let nonce = rand::random::<u32>();
+            let timestamp = std::time::SystemTime::now();
+            let content = Content(Vec::new());
+            let merkle_root = MerkleTree::new(&content.0.as_slice()).root();
+            let header = Header {
+                parent,
+                nonce,
+                difficulty,
+                timestamp,
+                merkle_root,
+            };
+
+            
+            let block = Block {header, content};
             // TODO for student: if block mining finished, you can have something like: self.finished_block_chan.send(block.clone()).expect("Send finished block error");
+            // println!("{}, {}", block.hash(), difficulty);
+            if block.hash() <= difficulty {
+                // println!("parent: {}", parent);
+                parent = block.hash();
+                match self.blockchain.lock() {
+                    Ok(mut blockchain_guard) => {
+                        blockchain_guard.insert(&block);
+                    }
+                    Err(poisoned) => {
+                        println!("The mutex is poisoned!");
+                    }
+                }
+                self.finished_block_chan.send(block.clone()).expect("Send finished block error");
+            }
 
             if let OperatingState::Run(i) = self.operating_state {
                 if i != 0 {
@@ -159,10 +208,12 @@ mod test {
         miner_ctx.start();
         miner_handle.start(0);
         let mut block_prev = finished_block_chan.recv().unwrap();
+        // println!("{}", block_prev.hash());
         for _ in 0..2 {
             let block_next = finished_block_chan.recv().unwrap();
             assert_eq!(block_prev.hash(), block_next.get_parent());
             block_prev = block_next;
+            // println!("{}", block_prev.hash());
         }
     }
 }
